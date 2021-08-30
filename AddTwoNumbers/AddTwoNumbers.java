feae661a09822d992c16ffb50c6a1b70f49bdd6d
Yohan Berga <yohan520@gmail.com>
2021-08-29 21:59:34 -0400

    Finished linked list functionality.

--- AddTwoNumbers/AddTwoNumbers.java
+++ AddTwoNumbers/AddTwoNumbers.java
@@ -2,24 +2,37 @@ import java.lang.Math;
 
 public class AddTwoNumbers {
     public static void main(String[] args) {
-        Node node = new Node(3);
+        Node node = createList(512);
+        printList(node);
     }
 
     public static void printList(Node head) {
-        if (head.next) printListRecur(head.next);
+        if (head.next != null) printListRecur(head.next);
         System.out.print(head.val);
         System.out.print("\n");
     }
 
     public static void printListRecur(Node node) {
-        if (node.next) printListRecur(node.next);
-        System.out.print(head.val);
+        if (node.next != null) printListRecur(node.next);
+        System.out.print(node.val);
     }
 
+    /**
+     * Creates a linked list from the given value
+     * 
+     * @param val the given value to be turned into a linked list.
+     * @return the head of the created linked list
+     */
     public static Node createList(int val) {
         Node head = null;
-        // Turn val into string
-        // Read string from left to right, creating the node with the read digit.
+        // Turn val into a character array.
+        char[] chars = ("" + val).toCharArray();
+        // Read character array from left to right, creating the node with the read digit.
+        for (int i = 0; i < chars.length; i++) {
+            Node new_node = new Node(Character.getNumericValue(chars[i]), head);
+            head = new_node;
+        }
+        if (head == null) return new Node(0);
         return head;
     }
 
